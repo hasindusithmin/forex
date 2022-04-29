5f647a015ea4cc0cf8473aab60999304671d5cb3
hasindusithmin <hasindusithmin64@gmail.com>
2022-04-29 15:55:00 +0530

    merge heroku git

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 from routers.yahoofinance import yf_router
 from routers.investing import invest_route
 
 description ="""
-Valid currency_cross for Both\n
+Code Unity Forex API helps you do awesome stuff. 🚀
+
+## Valid currency_cross for Both
+
 ['eurusd', 'usdjpy', 'gbpusd', 'audusd', 'nzdusd', 'eurjpy', 'gbpjpy', 'eurgbp', 'eurcad', 'eursek', 'eurchf', 'eurhuf', 'eurjpy', 'usdcny', 'usdhkd', 'usdsgd', 'usdinr', 'usdmxn', 'usdphp', 'usdidr', 'usdthb', 'usdmyr', 'usdzar', 'usdrub']\n
-Valid Interval for Yahoo Finance\n
-['5m','15m','30m','60m','1h']\n
-Valid Timeframe For investing.com\n
-['5mins', '15mins', '30mins','lhour']\n
-#github : https://github.com/hasindusithmin/forex.git
+
+## Valid Interval for Yahoo Finance
+
+['5m','15m','30m','60m','1h']
+
+## Valid Timeframe For investing.com
+
+['5mins', '15mins', '30mins','lhour']
+
+
 """
 
 app = FastAPI(
-    title="forexapi",
+    title="ForexApi",
     description=description,
-    version="0.0.1",
+    version="0.0.3",
     contact={
         "name": "Hasindu Sithmin",
         "email": "hasindusithmin64@gmail.com",
-    }
+    },
+    license_info={
+        "name": "github",
+        "url": "https://github.com/hasindusithmin/forex",
+    },
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get('/')
--- routers/yahoofinance.py
+++ routers/yahoofinance.py
@@ -7,25 +7,27 @@ yf_router = APIRouter(
     prefix="/yahoo",
     tags=["yahoo finance"]
 )
-currency_cross_list = ['EURUSD=X', 'JPY=X', 'GBPUSD=X', 'AUDUSD=X', 'NZDUSD=X', 'EURJPY=X', 'GBPJPY=X', 'EURGBP=X', 'EURCAD=X', 'EURSEK=X', 'EURCHF=X', 'EURHUF=X', 'EURJPY=X', 'CNY=X', 'HKD=X', 'SGD=X', 'INR=X', 'MXN=X', 'PHP=X', 'IDR=X', 'THB=X', 'MYR=X', 'ZAR=X', 'RUB=X']
+currency_cross_list = ['eurusd', 'usdjpy', 'gbpusd', 'audusd', 'nzdusd', 'eurjpy', 'gbpjpy', 'eurgbp', 'eurcad', 'eursek', 'eurchf', 'eurhuf', 'eurjpy', 'usdcny', 'usdhkd', 'usdsgd', 'usdinr', 'usdmxn', 'usdphp', 'usdidr', 'usdthb', 'usdmyr', 'usdzar', 'usdrub']
+currency_cross_dict = {'usdjpy': 'JPY=X', 'usdcny': 'CNY=X', 'usdhkd': 'HKD=X', 'usdsgd': 'SGD=X', 'usdinr': 'INR=X', 'usdmxn': 'MXN=X', 'usdphp': 'PHP=X', 'usdidr': 'IDR=X', 'usdthb': 'THB=X', 'usdmyr': 'MYR=X', 'usdzar': 'ZAR=X', 'usdrub': 'RUB=X', 'eurusd': 'EURUSD=X', 'gbpusd': 'GBPUSD=X', 'audusd': 'AUDUSD=X', 'nzdusd': 'NZDUSD=X', 'eurjpy': 'EURJPY=X', 'gbpjpy': 'GBPJPY=X', 'eurgbp': 'EURGBP=X', 'eurcad': 'EURCAD=X', 'eursek': 'EURSEK=X', 'eurchf': 'EURCHF=X', 'eurhuf': 'EURHUF=X'}
 interval_list = ['5m','15m','30m','60m','1h']
 @yf_router.get('/info/{currency_cross}')
 async def get_info(currency_cross:str):
-    currency_cross = f'{currency_cross.upper()}=X'
+    currency_cross = currency_cross.strip()
     valid_cross = True if currency_cross in currency_cross_list else False
     if not valid_cross:
         raise HTTPException(status_code=404, detail='sorry currency cross not found')
-    return yf.Ticker(currency_cross).info
+    return yf.Ticker(currency_cross_dict[currency_cross]).info
+
 @yf_router.get('/history/{currency_cross}')
 async def get_candlestick(currency_cross:str,interval:str="5m"):
-    currency_cross = f'{currency_cross.upper()}=X'
+    currency_cross = currency_cross.strip()
     valid_cross = True if currency_cross in currency_cross_list else False
     if not valid_cross:
         raise HTTPException(status_code=404, detail='sorry currency cross not found')
     valid_interval = True if interval in interval_list else False
     if not valid_interval:
         raise HTTPException(status_code=404, detail='sorry interval not found')
-    df = yf.Ticker(currency_cross).history(interval=interval,period="1mo")
+    df = yf.Ticker(currency_cross_dict[currency_cross]).history(interval=interval,period="1mo")
     df = df.tail(500)
     df = df.reset_index()
     del df['Volume']
@@ -42,8 +44,8 @@ async def get_candlestick(currency_cross:str,interval:str="5m"):
 
 @yf_router.get('/news/{currency_cross}')
 async def get_news(currency_cross:str):
-    currency_cross = f'{currency_cross.upper()}=X'
+    currency_cross = currency_cross.strip()
     valid_cross = True if currency_cross in currency_cross_list else False
     if not valid_cross:
         raise HTTPException(status_code=404, detail='sorry currency cross not found')
-    return yf.Ticker(currency_cross).news
+    return yf.Ticker(currency_cross_dict[currency_cross]).news
